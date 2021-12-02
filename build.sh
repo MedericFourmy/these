1d1d0858b8330b949ad245af0369ad8325ebf31f
Mederic Fourmy <mederic.fourmy@gmail.com>
2021-12-02 16:42:51 +0100

    build.sh : add option to avoid building with bibliography

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Parse input arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -n|--nojustin) nojustin=true;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 pdflatex these
-biber these
-pdflatex these
+
+# Do not update biblio to save time
+if [ "$nojustin" = false ]
+then
+    biber these
+    pdflatex these
+fi
